3ed80b316724c8d547d63e8aefcc7eb7d7ad30c7
Spinal <Spinal@Spinal-PC>
2016-11-29 22:53:43 -0500

    Change to Inventory

--- src/ez/Inventory.java
+++ src/ez/Inventory.java
@@ -12,9 +12,23 @@ package ez;
 public class Inventory 
 {
     private Inventory()
-    {}
+    {
+        profit = 0;
+        cost = 0;
+        revenue = 0;
+    }
+    
+    /**Method that will return the only instance of that class
+     * @return The only instance of the class
+     */
+    public Inventory getInstance()
+    {
+        return invt;
+    }
     
     
     private static Inventory invt = new Inventory(); 
-    
+    private double profit;
+    private double cost;
+    private double revenue;    
 }
