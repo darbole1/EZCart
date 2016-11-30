ed51950fa5b75aca461b44cbc28522423cae989f
Spinal <Spinal@Spinal-PC>
2016-11-29 22:00:52 -0500

    Starter Code for EZ Shopping

---
+++ src/ez/Account.java
@@ -0,0 +1,58 @@
+
+package ez;
+
+/**
+ *
+ * @author Spinal
+ */
+public class Account 
+{
+    /**Account constructor
+     * @param fName Customer's first name
+     * @param lName Customer's last name
+     * @param user Customer's username
+     * @param pwd Customer's password
+     * @param type The type of account: either Customer or Seller Account
+     * For Customer account, type = 0
+     * For Seller account, type = 1
+     */
+    public Account( String fName, String lName, String user, String pwd, int type )
+    {
+        firstName = fName;
+        lastName = lName;
+        userName = user;
+        password = pwd;
+        accountType = type;
+    }
+    
+    /**Returns the username for the current account
+     * @return username for the current account
+     */
+    public String getUserName()
+    {
+        return userName;
+    }
+    
+    /**Returns the password for the current account
+     * @return Password for the current account
+     */
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    /**Returns the account type to determine whether the current account
+     * is for a customer or a seller
+     * @return 0 if it is a customer's account, 1 if it is a seller account
+     */
+    public int getAccountType()
+    {
+        return accountType;
+    }
+            
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private int accountType;    
+}
---
+++ src/ez/Inventory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ez;
+
+/**
+ *
+ * @author Spinal
+ */
+public class Inventory 
+{
+    private Inventory()
+    {}
+    
+    
+    private static Inventory invt = new Inventory(); 
+    
+}
