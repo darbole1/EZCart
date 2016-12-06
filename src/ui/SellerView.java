885f43f652711301bdc82169425cfcdc1d2c62c7
Diego A <darbole1@fau.edu>
2016-12-06 01:43:05 -0500

    reformating and organizing functions in seller view

--- src/ui/SellerView.java
+++ src/ui/SellerView.java
@@ -1,7 +1,6 @@
 package ui;
 
 import ez.Inventory;
-import ez.InventoryItem;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.*;
@@ -17,6 +16,7 @@ import java.awt.event.ActionListener;
 public class SellerView extends JFrame {
 
     private JButton buttonUpdateCart, buttonLogOut, buttonRemoveProd, buttonAddNewProd;
+    private JButton buttonCalcCost, buttonCalcProfit, buttonCalcRevenue;
     private JLabel productNameLabel, loggedUser;
     private JTextField productDtlsTextField, pName, pDescription, pQuantity, pCost, pPrice;
     private boolean loggedIn;
@@ -70,12 +70,6 @@ public class SellerView extends JFrame {
         JPanel bottomPanel = new JPanel(new BorderLayout());
         panel.add(bottomPanel, BorderLayout.SOUTH);
 
-        JPanel leftPanel = new JPanel();
-        panel.add(leftPanel, BorderLayout.EAST);
-
-        JPanel rightPanel = new JPanel();
-        panel.add(rightPanel, BorderLayout.WEST);
-
         JPanel centerPanel = new JPanel();
         panel.add(centerPanel, BorderLayout.CENTER);
         getContentPane().add(panel);
@@ -90,12 +84,12 @@ public class SellerView extends JFrame {
             //add product details  text field
             productDtlsTextField = new JTextField();
             productDtlsTextField.setPreferredSize(new Dimension(350, 25));
-          
+
             centerPanel.add(productDtlsTextField);
 
-            buttonUpdateCart = new JButton("Update Product");
+            buttonUpdateCart = new JButton("Update");
             buttonUpdateCart.setPreferredSize(new Dimension(100, 25));
-            buttonUpdateCart.addActionListener(new onClickAddUpdateproduct());
+            buttonUpdateCart.addActionListener(new onClickOpenUpdateProductWindow());
             centerPanel.add(buttonUpdateCart);
 
             buttonRemoveProd = new JButton("Remove");
@@ -120,56 +114,88 @@ public class SellerView extends JFrame {
         buttonAddNewProd = new JButton("Add New Product");
         buttonAddNewProd.setPreferredSize(new Dimension(200, 25));
         buttonAddNewProd.setVisible(true);
-        buttonAddNewProd.addActionListener(new onClickOpenWindowToCreateNewProduct());
-        bottomPanel.add(buttonAddNewProd);
+        buttonAddNewProd.addActionListener(new onClickOpenCreateNewProduct());
+        bottomPanel.add(buttonAddNewProd, BorderLayout.NORTH);
+
+        //button to calculate revenue
+        buttonCalcRevenue = new JButton("Calculate Revenue");
+        buttonCalcRevenue.setPreferredSize(new Dimension(200, 25));
+        buttonCalcRevenue.setVisible(true);
+        buttonAddNewProd.addActionListener(new onClickCalculateRevenue());
+        bottomPanel.add(buttonCalcRevenue, BorderLayout.EAST);
+
+        //button to calculate cost
+        buttonCalcCost = new JButton("Calculate Cost");
+        buttonCalcCost.setPreferredSize(new Dimension(200, 25));
+        buttonCalcCost.setVisible(true);
+        buttonAddNewProd.addActionListener(new onClickCalculateCost());
+        bottomPanel.add(buttonCalcCost, BorderLayout.CENTER);
+
+        //button to calculate profit
+        buttonCalcProfit = new JButton("Calculate Profit");
+        buttonCalcProfit.setPreferredSize(new Dimension(200, 25));
+        buttonCalcProfit.setVisible(true);
+        buttonAddNewProd.addActionListener(new onClickCalculateProfit());
+        bottomPanel.add(buttonCalcProfit, BorderLayout.WEST);
+
         getContentPane().add(panel);
     }
 
     /**
-     * onClickAddProductToInventory
+     * onClickOpenUpdateProductWindow
      *
-     * @description calls
+     * @description Opens the pop-up that allows the customer/seller to update
+     * product details
      */
-    private class onClickAddUpdateproduct implements ActionListener {
+    private class onClickOpenUpdateProductWindow implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            pName = new JTextField();
-            pDescription = new JTextField();
-            pQuantity = new JTextField();
-            pCost = new JTextField();
-            pPrice = new JTextField();
 
-            pName.setPreferredSize(new Dimension(100, 25));
-            pDescription.setPreferredSize(new Dimension(100, 25));
-            pQuantity.setPreferredSize(new Dimension(100, 25));
-            pCost.setPreferredSize(new Dimension(100, 25));
-            pPrice.setPreferredSize(new Dimension(100, 25));
+            int result = createProductPopUp("Update Product Details");
 
-            JPanel mainPanel = new JPanel(new BorderLayout(5, 5));
-
-            JPanel labelsPanel = new JPanel(new GridLayout(0, 1, 2, 2));
-            labelsPanel.add(new JLabel("Please Update Product name: ", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Update Product Description:", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Update Product Quantity:", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Update Product Cost:", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Update Product Price:", SwingConstants.RIGHT));
-            mainPanel.add(labelsPanel, BorderLayout.WEST);
-
-            JPanel ProductPanel = new JPanel(new GridLayout(0, 1, 2, 2));
-            ProductPanel.add(pName);
-            ProductPanel.add(pDescription);
-            ProductPanel.add(pQuantity);
-            ProductPanel.add(pCost);
-            ProductPanel.add(pPrice);
-            mainPanel.add(ProductPanel, BorderLayout.CENTER);
-
-            int result = JOptionPane.showConfirmDialog(null, mainPanel,
-                    "Update Product Details", JOptionPane.OK_CANCEL_OPTION);
             if (result == JOptionPane.OK_OPTION) {
 
                 //add code here to update 
             }
+        }
+    }
+
+    /**
+     * onClickCalculateProfit
+     *
+     * @description O
+     */
+    private class onClickCalculateProfit implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    /**
+     * onClickCalculateCost
+     *
+     * @description Opens
+     */
+    private class onClickCalculateCost implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    /**
+     * onClickCalculateRevenue
+     *
+     * @description Opens
+     */
+    private class onClickCalculateRevenue implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
 
         }
     }
@@ -177,7 +203,8 @@ public class SellerView extends JFrame {
     /**
      * onClickLogOutOfSellerView
      *
-     * @description calls
+     * @description Logs out the user and removes permissions only available to
+     * seller
      */
     private class onClickLogOutOfSellerView implements ActionListener {
 
@@ -192,50 +219,65 @@ public class SellerView extends JFrame {
     }
 
     /**
+     * createProductPopUp
+     *
+     * @description creates a pop-up window which allows the customer/seller to
+     * add, update product details
+     * @params popUpTitle - the title which is assigned to the pop-up window
+     * @return userSelection - the choice selected by the customer/seller
+     */
+    public int createProductPopUp(String popUpTitle) {
+
+        pName = new JTextField();
+        pDescription = new JTextField();
+        pQuantity = new JTextField();
+        pCost = new JTextField();
+        pPrice = new JTextField();
+
+        pName.setPreferredSize(new Dimension(100, 25));
+        pDescription.setPreferredSize(new Dimension(100, 25));
+        pQuantity.setPreferredSize(new Dimension(100, 25));
+        pCost.setPreferredSize(new Dimension(100, 25));
+        pPrice.setPreferredSize(new Dimension(100, 25));
+
+        JPanel mainPanel = new JPanel(new BorderLayout(5, 5));
+
+        JPanel labelsPanel = new JPanel(new GridLayout(0, 1, 2, 2));
+        labelsPanel.add(new JLabel("Product name: ", SwingConstants.RIGHT));
+        labelsPanel.add(new JLabel("Product Description:", SwingConstants.RIGHT));
+        labelsPanel.add(new JLabel("Product Quantity:", SwingConstants.RIGHT));
+        labelsPanel.add(new JLabel("Product Cost:", SwingConstants.RIGHT));
+        labelsPanel.add(new JLabel("Product Price:", SwingConstants.RIGHT));
+        mainPanel.add(labelsPanel, BorderLayout.WEST);
+
+        JPanel newProductPanel = new JPanel(new GridLayout(0, 1, 2, 2));
+        newProductPanel.add(pName);
+        newProductPanel.add(pDescription);
+        newProductPanel.add(pQuantity);
+        newProductPanel.add(pCost);
+        newProductPanel.add(pPrice);
+        mainPanel.add(newProductPanel, BorderLayout.CENTER);
+
+        int userSelection = JOptionPane.showConfirmDialog(null, mainPanel,
+                popUpTitle, JOptionPane.OK_CANCEL_OPTION);
+        return userSelection;
+    }
+
+    /**
      * onClickOpenWindowToCreateNewProduct
      *
      * @description calls
      */
-    private class onClickOpenWindowToCreateNewProduct implements ActionListener {
+    private class onClickOpenCreateNewProduct implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            pName = new JTextField();
-            pDescription = new JTextField();
-            pQuantity = new JTextField();
-            pCost = new JTextField();
-            pPrice = new JTextField();
+            int result = createProductPopUp("Please Enter Product Details");
 
-            pName.setPreferredSize(new Dimension(100, 25));
-            pDescription.setPreferredSize(new Dimension(100, 25));
-            pQuantity.setPreferredSize(new Dimension(100, 25));
-            pCost.setPreferredSize(new Dimension(100, 25));
-            pPrice.setPreferredSize(new Dimension(100, 25));
-
-            JPanel mainPanel = new JPanel(new BorderLayout(5, 5));
-
-            JPanel labelsPanel = new JPanel(new GridLayout(0, 1, 2, 2));
-            labelsPanel.add(new JLabel("Please Enter new Product name: ", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Enter Product Description:", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Enter Product Quantity:", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Enter Product Cost:", SwingConstants.RIGHT));
-            labelsPanel.add(new JLabel("Please Enter Product Price:", SwingConstants.RIGHT));
-            mainPanel.add(labelsPanel, BorderLayout.WEST);
-
-            JPanel newProductPanel = new JPanel(new GridLayout(0, 1, 2, 2));
-            newProductPanel.add(pName);
-            newProductPanel.add(pDescription);
-            newProductPanel.add(pQuantity);
-            newProductPanel.add(pCost);
-            newProductPanel.add(pPrice);
-            mainPanel.add(newProductPanel, BorderLayout.CENTER);
-
-            int result = JOptionPane.showConfirmDialog(null, mainPanel,
-                    "Please Enter Product Details", JOptionPane.OK_CANCEL_OPTION);
             if (result == JOptionPane.OK_OPTION) {
 
                 inventory.addNewProduct(pName.getText(), Double.parseDouble(pCost.getText()), Double.parseDouble(pPrice.getText()), pDescription.getText(), Integer.parseInt(pQuantity.getText()));
-            productDtlsTextField.setText(pDescription.getText());
+                productDtlsTextField.setText(pDescription.getText());
             }
         }
     }
